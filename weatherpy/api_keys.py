4133e26ca41ec75395acc64d380e1ac19fef6fe7
Ana Stefanski <anastefanski@yahoo.com>
2021-07-23 18:31:05 -0400

    starter code

---
+++ weatherpy/api_keys.py
@@ -0,0 +1,35 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "7c38cd11-1eba-410c-9194-3f3406fe7a29",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "weather_api_key = \"bbf8730e341cdc5aa04ea1d7403f4cd9\""
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.7.10"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
