5d4228c8daea8e626e534e627cf942fb1c81a5d5
Ana Stefanski <anastefanski@yahoo.com>
2021-07-25 10:15:46 -0400

    weather

--- weatherpy/.ipynb_checkpoints/api_keys-checkpoint.py
+++ weatherpy/.ipynb_checkpoints/api_keys-checkpoint.py
@@ -1,6 +1,5 @@
-{
- "cells": [],
- "metadata": {},
- "nbformat": 4,
- "nbformat_minor": 5
-}
+# OpenWeatherMap API Key
+weather_api_key = "bbf8730e341cdc5aa04ea1d7403f4cd9"
+
+# Google API Key
+g_key = "AIzaSyDteZ4qvAgRPMFYtCoD5zDGahAhDscWb7k"
